a8d909e7109a042a5bcfd84b182aae79341757cb
Justin Zhao <justinzhao265@gmail.com>
2023-12-18 15:10:27 -0500

    removed tooltip on small screens + minor css adjustments

--- app/uid/[uid]/characterDetails.jsx
+++ app/uid/[uid]/characterDetails.jsx
@@ -137,8 +137,8 @@ export default function CharacterList({character, createAssetUrl}) {
           </div>
           <div>
             {Object.keys(relic_sets_data).map(set_id => 
-              <div key={"set-" + set_id} className="group flex justify-between text-sm bg-dark-grey/[0.6] rounded-lg p-1 mb-1">
-                <span className="absolute scale-0 translate-x-[19rem] bg-dark-grey/[0.95] z-10 p-2 rounded-md text-s text-white text-sm w-96 flex flex-col gap-4 group-hover:scale-100">{relic_sets.filter(set => set.id == set_id).map(set => <span key={set.id + set.num}>{set.desc}</span>)}</span>
+              <div key={"set-" + set_id} className="group flex justify-between text-sm bg-dark-grey/[0.6] rounded-lg px-2 py-1 mb-1">
+                <span className="absolute scale-0 translate-x-[19rem] bg-dark-grey/[0.95] z-10 p-3 rounded-md text-s text-white text-sm w-96 flex flex-col gap-4 md:group-hover:scale-100">{relic_sets.filter(set => set.id == set_id).map(set => <span key={set.id + set.num}>{set.desc}</span>)}</span>
                 <div>
                   {relic_sets_data[set_id].name}
                 </div>
@@ -162,7 +162,7 @@ export default function CharacterList({character, createAssetUrl}) {
               {rank_icons.map(((icon, index) => 
                 <div key={"eidolon-" + index} className="group mr-1">
                   {/* need eidolon data
-                  <span className="absolute scale-0 translate-x-12 bg-dark-grey/[0.95] z-10 p-2 rounded-md text-s text-white text-sm w-96 group-hover:scale-100">ok</span> */}
+                  <span className="absolute scale-0 translate-x-12 bg-dark-grey/[0.95] z-10 p-2 rounded-md text-s text-white text-sm w-96 md:group-hover:scale-100">ok</span> */}
                   <div className="h-11 w-11 rounded-full border border-white-500 p-1">
                     <img className="h-9 w-9" src={createAssetUrl(icon)} />
                   </div>
--- app/uid/[uid]/skillTrace.jsx
+++ app/uid/[uid]/skillTrace.jsx
@@ -4,7 +4,7 @@ export default function SkillTrace({ skill, createAssetUrl, type }) {
   const {name, icon, level, type_text} = skill[0]
   return (
     <div className={`group flex flex-col items-center relative ${type == "Ultimate"?"translate-y-14 z-[5]":null}`}>
-      <span className="flex flex-col gap-3 absolute scale-0 translate-x-56 bg-dark-grey/[0.95] z-10 p-2 rounded-md text-s text-white text-sm w-96 group-hover:scale-100">{
+      <span className="flex flex-col gap-3 absolute scale-0 md:translate-x-56 bg-dark-grey/[0.95] z-10 p-3 rounded-md text-s text-white text-sm w-96 md:group-hover:scale-100">{
         skill.map(s => 
           <div key={"trace-" + s.name} className="flex flex-col text-sm">
             <span className="mb-1 text-light-blue">
