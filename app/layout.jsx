003e1e9b91d184210563c6a297a0e1d2fb1c1e4d
Justin Zhao <justinzhao265@gmail.com>
2023-12-22 12:07:06 -0500

    analytics + updated next.config

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
